4e2572de3a8dc295f7acd9242530340244e4c3d5
Kanchan10Dhiman <kanchandhiman008@gmail.com>
2026-01-13 17:52:48 +0530

    added folder

---
+++ LinkedList/ll1.cpp
@@ -0,0 +1,13 @@
+//LINKED LIST
+#include<iostream>
+using namespace std;
+
+//creating node
+class node{
+    int data;
+    node *next;
+};
+int main(){
+    node *head=new node(); //made a pointer that store address of the node we made using new 
+    node *second=new node();
+}
